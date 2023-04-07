a53b2a50bd7550d158b0295f5c25ffa11dcb7d98
Gert Goet <gert@thinkcreate.dk>
2023-04-07 13:25:14 +0200

    deps-try v0.3.6

---
+++ Formula/deps-try.rb
@@ -0,0 +1,24 @@
+class DepsTry < Formula
+  desc "Try out Clojure libraries on rebel-readline"
+  homepage "https://github.com/eval/deps-try"
+  license "MIT"
+  url "https://github.com/eval/deps-try/releases/download/v0.3.6/deps-try.jar"
+  sha256 "8753ffbc526699744559177ac34bd6a1c74d991c82e387208b552cbaa2f7a738"
+
+  version "0.3.6"
+
+  depends_on "borkdude/brew/babashka"
+
+  def install
+    libexec.install "deps-try.jar"
+
+    (bin/"deps-try").write <<~EOS
+      #!/bin/sh
+      exec "#{Formula["babashka"].opt_bin}/bb" "#{libexec}/deps-try.jar"
+    EOS
+  end
+
+  test do
+    %x[deps-try -h]
+  end
+end
