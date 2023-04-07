7cef136142ea05225f990e3f49d3387d9734ef78
Gert Goet <gert@thinkcreate.dk>
2023-04-07 15:16:43 +0200

    Fix lint issues
    brew style --fix eval/brew

--- Formula/deps-try.rb
+++ Formula/deps-try.rb
@@ -1,14 +1,13 @@
 class DepsTry < Formula
   desc "Try out Clojure libraries on rebel-readline"
   homepage "https://github.com/eval/deps-try"
-  license "MIT"
   url "https://github.com/eval/deps-try/releases/download/v0.3.6/deps-try.jar"
-  sha256 "8753ffbc526699744559177ac34bd6a1c74d991c82e387208b552cbaa2f7a738"
-
   version "0.3.6"
+  sha256 "8753ffbc526699744559177ac34bd6a1c74d991c82e387208b552cbaa2f7a738"
+  license "MIT"
 
-  depends_on "clojure/tools/clojure"
   depends_on "borkdude/brew/babashka"
+  depends_on "clojure/tools/clojure"
 
   def install
     libexec.install "deps-try.jar"
@@ -20,6 +19,6 @@ class DepsTry < Formula
   end
 
   test do
-    %x[deps-try -h]
+    `deps-try -h`
   end
 end
