53a711076ace825631128097a98bfd471a4dbec2
Gert Goet <gert@thinkcreate.dk>
2023-04-07 22:06:05 +0200

    nightly -> unstable

--- Formula/deps-try.rb
+++ Formula/deps-try.rb
@@ -4,7 +4,7 @@ class DepsTry < Formula
   url "https://github.com/eval/deps-try/releases/download/v0.3.6/deps-try.jar"
   sha256 "8753ffbc526699744559177ac34bd6a1c74d991c82e387208b552cbaa2f7a738"
   license "MIT"
-  head "https://github.com/eval/deps-try/releases/download/nightly/deps-try.jar"
+  head "https://github.com/eval/deps-try/releases/download/unstable/deps-try.jar"
 
   depends_on "borkdude/brew/babashka"
   depends_on "clojure/tools/clojure"
