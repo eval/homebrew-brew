cd376060cbf441d0feebb357e9a749d9e5a4d181
Gert Goet <gert@thinkcreate.dk>
2023-04-15 09:46:33 +0200

    Back to using jar

--- Formula/deps-try.rb
+++ Formula/deps-try.rb
@@ -1,25 +1,20 @@
 class DepsTry < Formula
   desc "Try out Clojure libraries on rebel-readline"
   homepage "https://github.com/eval/deps-try"
-  url "https://github.com/eval/deps-try/releases/download/v0.3.9/deps-try.zip"
-  sha256 "d2ed407df7f77614e781e58718ba1027b987ae66fc2f2766953a0ac8c3155bb9"
+  url "https://github.com/eval/deps-try/releases/download/v0.3.9/deps-try-bb.jar"
+  sha256 "d8338c15db553880ec64afd20dbf524ba19a31c892adab0e5d5799073950fa98"
   license "MIT"
-  head "https://github.com/eval/deps-try/releases/download/unstable/deps-try.zip"
+  head "https://github.com/eval/deps-try/releases/download/unstable/deps-try-bb.jar"
 
   depends_on "borkdude/brew/babashka"
   depends_on "clojure/tools/clojure"
 
   def install
-    with_env(
-      "CLJ_CACHE" => ".cpcache",
-    ) do
-      system "bb", "uberjar", "deps-try.jar", "-m", "eval.deps-try"
-    end
-    libexec.install "deps-try.jar"
+    libexec.install "deps-try-bb.jar"
 
     (bin/"deps-try").write <<~EOS
       #!/bin/sh
-      exec "#{Formula["babashka"].opt_bin}/bb" "#{libexec}/deps-try.jar" "$@"
+      exec "#{Formula["babashka"].opt_bin}/bb" "#{libexec}/deps-try-bb.jar" "$@"
     EOS
   end
 
