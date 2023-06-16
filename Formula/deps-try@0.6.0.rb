c78e45ac8f93b5624988053b09e3e6ab543d900a
Gert Goet <gert@thinkcreate.dk>
2023-06-16 12:47:19 +0200

    v0.7.0

--- Formula/deps-try@0.6.0.rb
+++ Formula/deps-try@0.6.0.rb
@@ -1,8 +1,8 @@
-class DepsTry < Formula
+class DepsTryAT060 < Formula
   desc "Try out Clojure libraries on rebel-readline"
   homepage "https://github.com/eval/deps-try"
-  url "https://github.com/eval/deps-try/releases/download/v0.7.0/deps-try-bb.jar"
-  sha256 "0c247d688b386032a8df35dc3df7ca08881d16e11ab988d3e239cbdc8d7a003f"
+  url "https://github.com/eval/deps-try/releases/download/v0.6.0/deps-try-bb.jar"
+  sha256 "9e463dfb791d5518a3cac1c83e63025a1a94ba1587d890fe708f534c921a4e75"
   license "MIT"
   head "https://github.com/eval/deps-try/releases/download/unstable/deps-try-bb.jar"
 
