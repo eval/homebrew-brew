c7b67855da026ff54e43896b1d9de2802b9c4c32
Gert Goet <gert@thinkcreate.dk>
2023-11-21 12:10:54 +0100

    bbang v0.7.0

--- Formula/bbang@0.6.0.rb
+++ Formula/bbang@0.6.0.rb
@@ -1,8 +1,8 @@
-class Bbang < Formula
+class BbangAT060 < Formula
   desc "Render the README's Links-section in the terminal"
   homepage "https://github.com/eval/bbangsearch"
-  url "https://github.com/eval/bbangsearch/releases/download/v0.7.0/bbang-bb.jar"
-  sha256 "96e8b822bbb22e49c8aadb7d606cd92f62208108e0d486585bd20d4b613d2bb3"
+  url "https://github.com/eval/bbangsearch/releases/download/v0.6.0/bbang-bb.jar"
+  sha256 "5a108bc0e0e1c7e8d4be90b7f997f5d7d630c1215e75b36ed5c1b5fb3208cbaf"
   license "MIT"
   head "https://github.com/eval/bbangsearch/releases/download/unstable/bbang-bb.jar"
 
